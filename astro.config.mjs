b6018916ab52ceaedbdc43b4e1b043d6b061ec96
Ben Walters <ben@ben-air.lan>
2024-03-15 22:31:30 -0500

    Revert Astro config specific to Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,4 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: 'https://bendrick92.github.io',
-  base: '/e5-woodworx',
 });
